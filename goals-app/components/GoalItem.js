93b94821fd67e9b0c79f32b07e955a45ff018af9
Andres Velasquez <vlasquez53069@hotmail.com>
2024-12-12 20:33:09 -0500

    feature: implementation of modularization, creation of react components, usage of flatList to render a list of dynamic objects

---
+++ goals-app/components/GoalItem.js
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={{ color: "white", padding: 8 }}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+});
