add5be81c0fba1240ff356b522b7c276fe6e6d01
Andres Velasquez <vlasquez53069@hotmail.com>
2024-12-31 14:05:18 -0500

    feature: implementation of background image

--- guess-number-app/App.js
+++ guess-number-app/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screen/StartGameScreen";
@@ -6,7 +6,14 @@ import StartGameScreen from "./screen/StartGameScreen";
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.container}>
-      <StartGameScreen />;
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.container}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />;
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,5 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
