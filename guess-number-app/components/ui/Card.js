47d369c6f8315ed57a4e10bcdc474aa7cecd9a98
Andres Velasquez <vlasquez53069@hotmail.com>
2025-01-02 11:15:16 -0500

    feature: implementation of new components and creation of different states that handles game status

---
+++ guess-number-app/components/ui/Card.js
@@ -0,0 +1,25 @@
+import { StyleSheet, View } from "react-native";
+
+import Colors from "../../design-system/colors";
+function Card({ children }) {
+  return <View style={styles.card}>{children}</View>;
+}
+
+export default Card;
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    padding: 16,
+    marginTop: 36,
+    backgroundColor: Colors.primary800,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
