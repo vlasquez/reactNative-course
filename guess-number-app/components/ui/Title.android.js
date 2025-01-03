b2225e8e1bc59142e67949045c530d7b48e6466f
Andres Velasquez <vlasquez53069@hotmail.com>
2025-01-03 17:26:32 -0500

    feature: usage of statusbar component

---
+++ guess-number-app/components/ui/Title.android.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, Platform } from "react-native";
+
+function Title({ children }) {
+  return (
+    <View>
+      <Text style={styles.title}>{children}</Text>
+    </View>
+  );
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    //borderWidth: Platform.OS === "android" ? 2 : 0,
+    borderWidth: Platform.select({ ios: 2, android: 0 }),
+    borderColor: "white",
+    padding: 12,
+    maxWidth: "80%",
+  },
+});
